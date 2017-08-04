3e75e423d092e4d8d33d4aa22308cc038f923222
Gustavo Arjones <garjones@socialmetrix.com>
2017-08-04 18:09:54 +0000

    Play v2 images

---
+++ build-all-play2.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+read -r -d '' PLAY_VERSIONS <<-LIST
+2.0.8
+2.1.5
+LIST
+
+for PLAY_VERSION in $(echo $PLAY_VERSIONS); do
+  docker build --build-arg PLAY_VERSION=${PLAY_VERSION} -t socialmetrix/play:${PLAY_VERSION} .
+done
+
+docker images socialmetrix/play
+
+# Push to socialmetrix/play repo
+for PLAY_VERSION in $(echo $PLAY_VERSIONS); do
+  docker push socialmetrix/play:${PLAY_VERSION}
+done
